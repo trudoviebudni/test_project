b4226cab28655863648ece795b6aa0778d728adc
trudoviebudni <mumkoo@gmail.com>
2021-03-01 21:23:00 +0300

    initial commit

--- stepik_tours/tours/views.py
+++ stepik_tours/tours/views.py
@@ -4,6 +4,7 @@ from tours.data import *
 from django.views import View
 from django.shortcuts import render
 import re
+from django.views.generic import ListView
 
 
 
@@ -42,6 +43,7 @@ def custom_handler404(request, exception):
 
 
 class MainView(View):
+
     def get(self, request, *args, **kwargs):
         context = {}
         context['tours_list'] = tours_list
@@ -49,13 +51,11 @@ class MainView(View):
         context['subtitle'] = subtitle
         context['description'] = description
         context['departures'] = departures
-        if re.findall(r'8000\b', request.build_absolute_uri()):
-            return render(request, 'index.html', context=context)
-        else:
-            return HttpResponseNotFound('Ой, что то сломалось... Простите извините!')
+        return render(request, 'index.html', context=context)
 
 
 class TourView(View):
+
     def get(self, request, *args, **kwargs):
         x = re.findall(r'(?:.+/tour/)(\d+)', request.build_absolute_uri())[0]
         x = int(x)
@@ -71,20 +71,23 @@ class TourView(View):
 
 
 class DepartureView(View):
+
     def get(self, request, *args, **kwargs):
-        x = re.findall(r"(?:.+/departure/)(\w+)", request.build_absolute_uri())[0]
+        #x = re.findall(r"(?:.+/departure/)(\w+)", request.build_absolute_uri())[0]
         context = {}
-        context['departure'] = departures[x]
+        #context['departure'] = departures[x]
+        context['departure'] = departures[kwargs['departure']]
         context['tours_list'] = tours_list
         context['title'] = title
         context['subtitle'] = subtitle
         context['description'] = description
         context['departures'] = departures
-        context['tours'] = [tours[key] for key in tours if tours[key]['departure'] == x]
+        context['tours'] = [tours[key] for key in tours if tours[key]['departure'] == kwargs['departure']]
         context['size'] = len(context['tours'])
         context['price_min'] = min(elem['price'] for elem in context['tours'])
         context['price_max'] = max(elem['price'] for elem in context['tours'])
         context['nights_min'] = min(elem['nights'] for elem in context['tours'])
         context['nights_max'] = max(elem['nights'] for elem in context['tours'])
-
         return render(request, 'departure.html', context=context)
+
+
